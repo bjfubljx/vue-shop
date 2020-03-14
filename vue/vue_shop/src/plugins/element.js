ad5a42349666cb36cd62956ec66ef3a11a602c56
bjfubljx <3488731470@qq.com>
2020-03-14 21:22:50 +0800

    完成订单功能开发

--- vue/vue_shop/src/plugins/element.js
+++ vue/vue_shop/src/plugins/element.js
@@ -1,44 +1,46 @@
 import Vue from 'vue'
-import { 
-    Button, 
-    Form, 
-    FormItem, 
-    Input, 
-    Message, 
-    Container, 
-    Header, 
-    Aside, 
-    Main, 
-    Menu, 
-    Submenu, 
-    MenuItem, 
-    Breadcrumb, 
-    BreadcrumbItem,
-    Card,
-    Row,
-    Col,
-    Table,
-    TableColumn,
-    Switch,
-    Tooltip,
-    Pagination,
-    DiaDialog,
-    Dialog,
-    MessageBox,
-    Tag,
-    Tree,
-    Select,
-    Option,
-    Cascader,
-    Alert,
-    Tabs,
-    TabPane,
-    Step,
-    Steps,
-    CheckboxGroup,
-    Checkbox,
-    Upload
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Row,
+  Col,
+  Table,
+  TableColumn,
+  Switch,
+  Tooltip,
+  Pagination,
+  Dialog,
+  MessageBox,
+  Tag,
+  Tree,
+  Select,
+  Option,
+  Cascader,
+  Alert,
+  Tabs,
+  TabPane,
+  Steps,
+  Step,
+  CheckboxGroup,
+  Checkbox,
+  Upload,
+  Timeline,
+  TimelineItem
 } from 'element-ui'
+
 
 Vue.use(Button)
 Vue.use(Form)
@@ -70,10 +72,13 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
-Vue.use(Step)
 Vue.use(Steps)
-Vue.use(Checkbox)
+Vue.use(Step)
 Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
+
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
